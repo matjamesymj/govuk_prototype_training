757b7fe109f488ce64f94c6923f1c4419f10af5a
Mathew James <mathew.james@dvla.gsi.gov.uk>
2017-10-25 11:36:27 +0100

    added https stuff

--- app/config.js
+++ app/config.js
@@ -16,5 +16,7 @@ module.exports = {
   // Cookie warning - update link to service's cookie page.
   cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#" title="Find out more about cookies">Find out more about cookies</a>'
 
+  // Force HTTP to redirect to HTTPs on production
+  useHttps: 'true',
+  
 };
-	
